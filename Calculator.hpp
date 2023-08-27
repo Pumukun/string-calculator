aa48afe7275e060bddf289e54b37d92b75d12c1b
pumukun <demgrisha@gmail.com>
2023-08-27 14:18:26 +0300

    added first bits

---
+++ Calculator.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <iostream>
+#include <stack>
+#include <cmath>
+
+using namespace std;
+
+class Calculator {
+public:
+
+    string expression;
+    
+    Calculator(): expression("") {}
+
+    Calculator(string expression): expression(expression) {}
+
+    ~Calculator() {}
+
+private:
+    
+
+
+};
